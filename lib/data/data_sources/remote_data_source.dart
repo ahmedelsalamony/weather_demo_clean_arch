720f1ae1c13893135a32de7a4bee13b31e626d76
ahmedelsalamony <ahmedabdelhameed79@gmail.com>
2022-09-12 11:46:09 +0200

    presentation layer

--- lib/data/data_sources/remote_data_source.dart
+++ lib/data/data_sources/remote_data_source.dart
@@ -13,8 +13,9 @@ class RemoteWeatherDataSource extends BaseRemoteWeatherDataSource{
   @override
   Future<WeatherModel?> getWeatherFromRemote(String countryName) async {
       try{
-        var response = await Dio().get(EndPoints.weatherByCountryNameApi+ '&appid='+ AppKeys.apiKey);
-        print(response);
+        var response = await Dio().get(
+        '${EndPoints.weatherByCountryNameApi}$countryName&appid=${AppKeys.apiKey}');
+        print(response.data);
         return WeatherModel.fromJson(response.data);
       } catch(e){
         print(e);
